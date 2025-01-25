9c58e99b235166bfed8dc891f336c875180387f3
taxicomics <168220579+taxicomics@users.noreply.github.com>
2025-01-25 20:32:46 +0100

    Update draw_sprite_frames.lua
    added a PICO-8 version

--- lua_scripts/draw_sprite_frames.lua
+++ lua_scripts/draw_sprite_frames.lua
@@ -1,5 +1,13 @@
+--Picotron variant
 function spr_frames(frames,x,y,speed,flip_x_bool,flip_y_bool)
 	local nr_of_frames=#frames
 	local this_frame=flr((time()*speed)%nr_of_frames)+1
 	spr(frames[this_frame],x,y,flip_x_bool,flip_y_bool)
+end
+
+--PICO-8 variant
+function spr_frames(frames,pos_x,pos_y,speed,x_size,y_size,flip_x_bool,flip_y_bool)
+	local nr_of_frames=#frames
+	local this_frame=flr((time()*speed)%nr_of_frames)+1
+	spr(frames[this_frame],pos_x,pos_y,x_size,y_size,flip_x_bool,flip_y_bool)
 end
